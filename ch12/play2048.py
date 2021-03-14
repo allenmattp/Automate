03d763de115f3a476c001c4fe1f0b4b98a6918f7
Matt <allen.mattp@gmail.com>
2021-03-14 14:36:18 -0700

    play 2048 like a dumb robot. counts wins and high score.

--- ch12/play2048.py
+++ ch12/play2048.py
@@ -12,7 +12,7 @@ htmlElem = driver.find_element_by_tag_name("html")
 
 def play_dumb():
     gameOver = []
-    while len(gameOver) < 1:    # keep pressing keys until game over message
+    while len(gameOver) < 1:  # keep pressing keys until game over message is found
         gameOver = driver.find_elements_by_class_name("game-message.game-over")
         htmlElem.send_keys(Keys.UP)
         htmlElem.send_keys(Keys.RIGHT)
@@ -20,11 +20,18 @@ def play_dumb():
         htmlElem.send_keys(Keys.LEFT)
 
 count = 0
-while count < 100:
+wins = 0
+while count < 100:  # specify number of rounds
     play_dumb()
+    score = driver.find_elements_by_class_name("score-container")   # get current score element
+    score_list = score[0].text.split()                              # grab only relevant text
+    if int(score_list[0]) >= 2048:                                  # check to see if it was a win
+        wins += 1
+
     count += 1
+
     retryElem = driver.find_element_by_class_name("retry-button")
     retryElem.click()
 
-bestScore = driver.find_elements_by_class_name("best-container")  # find high score
-print(f"Computer played {count} games and managed a high score of {bestScore[0].text}")
+bestScore = driver.find_elements_by_class_name("best-container")    # find high score
+print(f"Computer played {count} games and managed a high score of {bestScore[0].text}. It achieved 2048 {wins} times.")
