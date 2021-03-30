d52a4593c61fcd609bd7b9403442a79b1849c5ef
Matt <allen.mattp@gmail.com>
2021-03-29 17:53:19 -0700

    send email via gmail

--- ch18/emailPractice.py
+++ ch18/emailPractice.py
@@ -1,8 +1,11 @@
 import smtplib, passwordGen
 
+# SMTP object to connect to gmail mail server on port 587
 conn = smtplib.SMTP("smtp.gmail.com", 587)
 
+# say hello to server
 conn.ehlo()
+# start enable TLS encryption
 conn.starttls()
 
 # grab my app password so I don't upload it to github
@@ -13,3 +16,5 @@ conn.login("allenmattpdev@gmail.com", password)
 conn.sendmail("allenmattpdev@gmail.com", "allen.mattp@gmail.com",
               "Subject: Python test email\n\n"
               "This is a test email.")
+
+conn.quit()
