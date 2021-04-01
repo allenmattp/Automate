b64b3b59745b9452d96387d3ad685325e8d4b795
Matt <allen.mattp@gmail.com>
2021-03-31 19:54:59 -0700

    send email based on spreadsheet values

--- ch18/sendDuesReminders.py
+++ ch18/sendDuesReminders.py
@@ -1,19 +1,40 @@
 #! python3
 # sendDuesReminders.py - Sends emails based on payment status in spreadsheet
 
-import openpyxl, smtplib, sys
+import openpyxl, smtplib, passwordGen
 
 
 # Open spreadsheet and check dues status
 wb = openpyxl.load_workbook("duesRecords.xlsx")
-ws = wb.get_sheet_by_name("Sheet1")
+ws = wb["Sheet1"]
 lastCol = ws.max_column
 latestMonth = ws.cell(row=1, column=lastCol).value
 
-# TODO: Check each member's payment status
+# Check each member's payment status
+unpaidMembers = {}
+for r in range(2, ws.max_row + 1):
+    payment = ws.cell(row=r, column=lastCol).value      # find payment status of latest month
+    if payment != "paid":                               # check payment status
+        name = ws.cell(row=r, column=1).value           # member name stored in first column
+        email = ws.cell(row=r, column=2).value          # member email stored in second column
+        unpaidMembers[name] = email
 
+# Log into email account
+connSmtp = smtplib.SMTP("smtp.gmail.com", 587)
+connSmtp.ehlo()
+connSmtp.starttls()
+connSmtp.login("allenmattpdev@gmail.com", passwordGen.passGen())
 
-# TODO: Log into email account
+# Send out reminder emails
+for name, email in unpaidMembers.items():
+    body = f"Subject: {latestMonth} dues unpaid.\n" \
+           f"Dear {name}:\n" \
+           f"Records show that you have not paid dues for {latestMonth}.\n" \
+           f"Please make this payment ASAP."
+    print(f"Sending email to {email} ...")
+    sendStatus = connSmtp.sendmail("allenmattpdev@gmail.com", email, body)
 
+    if sendStatus != {}:
+        print(f"There was a problem sending email to {email}: {sendStatus}")
 
-# TODO: Send out reminder emails
+connSmtp.quit()
