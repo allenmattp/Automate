8f110cfe3e6c18eafe3e19848bc7da5a5d864a69
Matt <allen.mattp@gmail.com>
2021-04-08 19:36:57 -0700

    control mouse via PyAutoGUI

--- Ch20/roboGUI.py
+++ Ch20/roboGUI.py
@@ -7,19 +7,29 @@ print(wh.width)
 
 # return position of cursor at time function is called
 for i in range(5):
-    print(pyautogui.position())
     time.sleep(.25)
+    print(pyautogui.position())
 
 # move mouse on absolute coordinates
-for i in range(5):
+for i in range(2):
     pyautogui.moveTo(100, 100, duration=.25)
+    print(pyautogui.position())
     pyautogui.moveTo(200, 100, duration=.25)
+    print(pyautogui.position())
     pyautogui.moveTo(200, 200, duration=.25)
+    print(pyautogui.position())
     pyautogui.moveTo(100, 200, duration=.25)
+    print(pyautogui.position())
 
 # move mouse on relative coordinates
-for i in range(5):
+for i in range(2):
     pyautogui.move(100, 0, duration=.25)
+    print(pyautogui.position())
     pyautogui.move(0, 100, duration=.25)
+    print(pyautogui.position())
     pyautogui.move(-100, 0, duration=.25)
+    print(pyautogui.position())
     pyautogui.move(0, -100, duration=.25)
+    print(pyautogui.position())
+
+pyautogui.click(10, 5)      # Move mouse to (10, 5) and click
